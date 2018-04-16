a1eddd55cdb83860247be6572eed2b9c85a41e95
chengxueyan1122 <cheng1122xueyan@163.com>
2018-04-16 10:32:08 +0800

    周考一第二次

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minifyCss = require("gulp-minify-css");
+var minifyHtml = require("gulp-minify-html");
+var connect = require('gulp-connect')
